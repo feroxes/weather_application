791609b810b711476dfe5b954a7c982f6aebf95b
Yaroslav <harmash@kai24.com>
2019-09-04 19:11:07 +0300

    added moon&sun

--- src/components/ScreenWrapper/index.js
+++ src/components/ScreenWrapper/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './screenWrapper.scss';
+import Zoom from 'react-reveal/Bounce';
 
 function ScreenWrapper({ dayTime, children }) {
   return (
     <div className={`screen-wrapper screen-wrapper-${dayTime}`}>
-      {/*<div className={`star star-${dayTime}`}/>*/}
+      <Zoom>
+        <div className={`star star-${dayTime}`}>
+          <div className="inside-circle" />
+        </div>
+      </Zoom>
       {children}
     </div>
   );
