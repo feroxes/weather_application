a41ce5595535637d6cb738cf4cd60d40c573d9ad
Yaroslav Harmash <yaroslav.harmash@plus4u.net>
2020-12-09 16:04:34 +0200

    added style component to cloud.js

--- src/assets/styles/Themes.js
+++ src/assets/styles/Themes.js
@@ -1,11 +1,17 @@
 import DayBackground from '../images/screen/screen-background-day-2.png';
 import NightBackground from '../images/screen/screen-background-night-2.png';
 
+const Colors = {
+  white: '#fcf8f1',
+  lightBlue: '#a9dff7'
+};
+
 export const Themes = {
   day: {
     name: 'day',
     backgroundImage: DayBackground,
     backgroundWrapper: 'linear-gradient(180deg, #fcf8f1, #fcd1cb) center no-repeat',
+    cloudBackgroundColor: Colors.white,
     star: {
       backgroundColor: '#fef9e5'
     }
@@ -14,6 +20,7 @@ export const Themes = {
     name: 'night',
     backgroundImage: NightBackground,
     backgroundWrapper: 'linear-gradient(180deg, #39346a, #a9dff7) center no-repeat;',
+    cloudBackgroundColor: Colors.lightBlue,
     star: {
       boxShadow: '15px 15px 0 0 #e6eef2',
       right: '60px'
--- src/components/cloud/cloud.js
+++ src/components/cloud/cloud.js
@@ -1,9 +1,37 @@
 import React from 'react';
-import './cloud.scss';
-import { useSelector } from 'react-redux';
+import styled from 'styled-components';
 
-export const Cloud = ({ top, left, right }) => {
-  const dayTime = useSelector(state => state.app.dayTime);
+const CloudStyled = styled.div`
+  width: 12vw;
+  height: 12vw;
+  border-radius: 100%;
+  position: absolute;
+  background-color: ${props => props.theme.cloudBackgroundColor};
+  top: ${props => props.top};
+  left: ${props => props.left};
+  right: ${props => props.right};
+  &:before {
+    content: '';
+    display: block;
+    width: 9vw;
+    height: 9vw;
+    border-radius: 100%;
+    position: absolute;
+    top: 2vw;
+    right: 8vw;
+    background-color: ${props => props.theme.cloudBackgroundColor};
+  }
+  &:after {
+    content: '';
+    display: block;
+    width: 9vw;
+    height: 9vw;
+    border-radius: 100%;
+    position: absolute;
+    top: 2vw;
+    left: 8vw;
+    background-color: ${props => props.theme.cloudBackgroundColor};
+  }
+`;
 
-  return <div className={`cloud cloud-${dayTime}`} style={{ top, left, right }} />;
-};
+export const Cloud = ({ top, left, right }) => <CloudStyled top={top} left={left} right={right} />;
