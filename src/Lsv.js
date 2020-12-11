46d1ad7a69379dacacd7b7ade0b733f90b699a71
Yaroslav Harmash <yaroslav.harmash@plus4u.net>
2020-12-11 15:51:25 +0200

    created lsv file

---
+++ src/Lsv.js
@@ -0,0 +1,82 @@
+import { DetailsSectionTitleStyled } from './containers/details-screen/styled.js';
+
+export const Lsv = {
+  searchScreen: {
+    header: {
+      en: 'Search city by name',
+      ua: 'Пошук міста за назвою'
+    },
+    enterCity: {
+      en: 'Enter city name...',
+      ua: 'Введіть назву міста...'
+    },
+    poweredBy: {
+      en: 'Powered by Dark Sky',
+      ua: 'Дані надав Dark Sky'
+    }
+  },
+  detailScreen: {
+    details: {
+      en: 'Details',
+      ua: 'Деталі'
+    },
+    humidity: {
+      en: 'Humidity',
+      ua: 'Вологість'
+    },
+    cloudiness: {
+      en: 'Cloudiness',
+      ua: 'Хмарність'
+    },
+    visibility: {
+      en: 'Visibility',
+      ua: 'Видимість'
+    },
+    uvIndex: {
+      en: 'UV Index',
+      ua: 'Індекс УФ'
+    },
+    precipitation: {
+      en: 'Precipitation',
+      ua: 'Опади'
+    },
+    morning: {
+      en: 'Morning',
+      ua: 'Ранок'
+    },
+    midday: {
+      en: 'Midday',
+      ua: 'Середина дня'
+    },
+    evening: {
+      en: 'Evening',
+      ua: 'Вечір'
+    },
+    night: {
+      en: 'Night',
+      ua: 'Ніч'
+    },
+    windPressure: {
+      en: 'Wind & Pressure',
+      ua: 'Вітер та Тиск'
+    },
+    mps: {
+      en: 'mps',
+      ua: 'м/c'
+    },
+    barometer: {
+      en: 'Barometer',
+      ua: 'Барометр'
+    },
+    inches: {
+      en: 'inches',
+      ua: 'дюймів'
+    }
+  },
+  loadingSpinner: {
+    loading: {
+      en: 'Loading...',
+      ua: 'Іде завантаження...'
+    }
+  }
+};
