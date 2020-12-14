6e831f5b225265acc8246d178b0fdde525715bf3
Yaroslav Harmash <yaroslav.harmash@plus4u.net>
2020-12-14 18:01:06 +0200

    Updates

---
+++ precache-manifest.adf61a6216df264abe65957049c42d65.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3139b62cb7e97363229c600eb2260672",
+    "url": "/weather_application/index.html"
+  },
+  {
+    "revision": "99c3fc8c18432b7fd232",
+    "url": "/weather_application/static/css/main.92674cd6.chunk.css"
+  },
+  {
+    "revision": "c7998f20636532475bb2",
+    "url": "/weather_application/static/js/2.a04ee003.chunk.js"
+  },
+  {
+    "revision": "99c3fc8c18432b7fd232",
+    "url": "/weather_application/static/js/main.9d97dab1.chunk.js"
+  },
+  {
+    "revision": "296eeb9a0e3b1acd9d4c",
+    "url": "/weather_application/static/js/runtime~main.4a20f671.js"
+  },
+  {
+    "revision": "a8f8519444ad397cc6f0854972a090cb",
+    "url": "/weather_application/static/media/PFDinTextPro-Bold.a8f85194.ttf"
+  },
+  {
+    "revision": "91ed9a01fbe8936ffd81d0b62400c999",
+    "url": "/weather_application/static/media/PFDinTextPro-Medium.91ed9a01.ttf"
+  },
+  {
+    "revision": "26dee5b350ea3f2f8e0f39ba17144e7a",
+    "url": "/weather_application/static/media/PFDinTextPro-Regular.26dee5b3.ttf"
+  },
+  {
+    "revision": "ca0c8d76771ee66f274ea8aa4f4c8b10",
+    "url": "/weather_application/static/media/PFDinTextPro-Thin.ca0c8d76.ttf"
+  },
+  {
+    "revision": "b0eee8328f8fa46f0ce847f1932c3801",
+    "url": "/weather_application/static/media/PFDinTextPro-XThin.b0eee832.ttf"
+  },
+  {
+    "revision": "368636f1b6e330a4806185cdf6bb44bc",
+    "url": "/weather_application/static/media/SFProDisplay-Bold.368636f1.ttf"
+  },
+  {
+    "revision": "eebf2894b069d9cb93a29dab12aa31f5",
+    "url": "/weather_application/static/media/SFProDisplay-Light.eebf2894.ttf"
+  },
+  {
+    "revision": "c5a8e1f150a5b6d6d37e0f95220ed5f6",
+    "url": "/weather_application/static/media/SFProDisplay-Medium.c5a8e1f1.ttf"
+  },
+  {
+    "revision": "6987bcc482500f459516dc0342836ee5",
+    "url": "/weather_application/static/media/SFProDisplay-Regular.6987bcc4.ttf"
+  },
+  {
+    "revision": "ee12fa3bb3d015b1c1c30e15431eb95e",
+    "url": "/weather_application/static/media/eagle.ee12fa3b.gif"
+  },
+  {
+    "revision": "1e4b0174ed4d85df373e759a06a387e6",
+    "url": "/weather_application/static/media/left-hill-day.1e4b0174.png"
+  },
+  {
+    "revision": "6ad143494477986b3d893a2d83c9eb92",
+    "url": "/weather_application/static/media/left-hill-night.6ad14349.png"
+  },
+  {
+    "revision": "700daa44e4563918cae98baf4e6dfb7a",
+    "url": "/weather_application/static/media/right-hill-day.700daa44.png"
+  },
+  {
+    "revision": "c34e1117c5979912626bef015cad790f",
+    "url": "/weather_application/static/media/right-hill-night.c34e1117.png"
+  },
+  {
+    "revision": "a339fd3ddd157df28cb7a2e9c0e205ed",
+    "url": "/weather_application/static/media/screen-background-day-2.a339fd3d.png"
+  },
+  {
+    "revision": "2b065fc59d58ba2694360e992e2d6a5a",
+    "url": "/weather_application/static/media/screen-background-night-2.2b065fc5.png"
+  },
+  {
+    "revision": "cd99a9d104c63cc66dd01fe51c87bbf5",
+    "url": "/weather_application/static/media/windmill-fast.cd99a9d1.gif"
+  },
+  {
+    "revision": "08bd1cb7e95935a70fb2a0b944ec548e",
+    "url": "/weather_application/static/media/windmill-mid.08bd1cb7.gif"
+  },
+  {
+    "revision": "0544727f8f84e10dff96f475617d87c8",
+    "url": "/weather_application/static/media/windmill-slow.0544727f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weather_application/precache-manifest.88d7bcccd7e420676cddaf0d4287dbc4.js"
+  "/weather_application/precache-manifest.adf61a6216df264abe65957049c42d65.js"
 );
 
 self.addEventListener('message', (event) => {
