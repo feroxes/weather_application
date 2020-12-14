42296a9e2f5de82765b5a77918239f77f02cac66
Yaroslav Harmash <yaroslav.harmash@plus4u.net>
2020-12-14 17:59:29 +0200

    removed logger

--- src/store.js
+++ src/store.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
 import { watchLoadData } from './saga/sagas.js';
 
 import rootReducer from './reducers';
@@ -8,7 +7,7 @@ import rootReducer from './reducers';
 const sagaMiddleware = createSagaMiddleware();
 
 export default () => {
-  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+  const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
   sagaMiddleware.run(watchLoadData);
   return store;
 };
